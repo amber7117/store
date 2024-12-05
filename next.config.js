12eda7f752953bb0b8121fb1407b0447f5240b7a
amber7117 <amberbeaver@outlook.com>
2024-12-06 02:24:17 +0800

    ok

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-const runtimeCaching = require("next-pwa/cache");
-const nextTranslate = require("next-translate");
+import runtimeCaching from "next-pwa/cache";
+import nextTranslate from "next-translate";
 
 const withPWA = require("next-pwa")({
   dest: "public",
